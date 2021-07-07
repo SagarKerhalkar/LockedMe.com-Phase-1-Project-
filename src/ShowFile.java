da0f82b0b7edb29399aaf4f00bf3cae179acf58c
Sagar Kerhalkar <sagarhatta@gmail.com>
2021-07-07 16:09:54 +0530

    1 Sprint Work

---
+++ src/ShowFile.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ShowFile {
+   // public ShowFile() {
+    public static void main(String[] args) {
+        File FileDir = new File("D:\\LockedMe");
+        if (FileDir.isDirectory()){
+            List listFile = Arrays.asList(FileDir.list());
+            System.out.println("All File Name Are - ");
+//            for ( Object S : listFile){
+//                System.out.println(S);
+//            }
+//            System.out.println("________________||_____________________");
+            Collections.sort(listFile);
+            for (Object S : listFile){
+                System.out.println(S);
+            }
+        }
+        else {
+            System.out.println(FileDir.getAbsolutePath() + " Please Give correct Path of Directry ");
+        }
+
+    }
+}
